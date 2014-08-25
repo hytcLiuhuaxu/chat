839e76f1f3c70b0b63ec928d5705ab168149a7b8
Liuhuxu <407498779@qq.com>
2014-08-25 16:52:30 +0800

    2014.8.25
    局域网聊天

---
+++ hytc.demo/hytc.demo/hytc.demo/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace hytc.demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtmsg = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(197, 332);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "发送";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP:";
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(59, 44);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(114, 21);
+            this.txtIP.TabIndex = 2;
+            this.txtIP.Text = "255.255.255.255";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 80);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(260, 246);
+            this.textBox2.TabIndex = 3;
+            // 
+            // txtmsg
+            // 
+            this.txtmsg.Location = new System.Drawing.Point(12, 334);
+            this.txtmsg.Name = "txtmsg";
+            this.txtmsg.Size = new System.Drawing.Size(179, 21);
+            this.txtmsg.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "昵称：";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(59, 10);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(114, 21);
+            this.txtName.TabIndex = 6;
+            this.txtName.Text = "小苹果";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 367);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtmsg);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtmsg;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtName;
+    }
+}
+
---
+++ hytc.demo/hytc.demo/hytc.demo/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace hytc.demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+
+            //创建线程 开始线程
+            Form1.CheckForIllegalCrossThreadCalls = false;
+            Thread mythread = new Thread(new ThreadStart(listen));
+            mythread.IsBackground = true;
+            mythread.Start();
+
+            //打开程序对方显示登录
+            string ip = this.txtIP.Text;
+            UdpClient uc = new UdpClient();
+            string msg2 = "INROOM|" + this.txtName.Text;
+            byte[] bmsg2 = Encoding.Default.GetBytes(msg2);
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Parse(ip), 9527);
+            uc.Send(bmsg2, bmsg2.Length, ipep);
+        }
+        /// <summary>
+        ///进行监听
+        /// </summary>
+        private void listen()
+        {
+             UdpClient uc = new UdpClient(9527);
+                while (true)
+                {
+                    IPEndPoint ipep = new IPEndPoint(IPAddress.Any,9527);
+                    byte[] bmsg= uc.Receive(ref ipep);
+                    string msg = Encoding.Default.GetString(bmsg);
+                    string[] smsg = msg.Split('|');
+                    if (smsg[0] == "PUBLIC")
+                    {
+                        this.textBox2.Text +=smsg[2]+":"+smsg[1] + "\r\n";
+                    }
+                    if (smsg[0] == "INROOM")
+                    {
+                        this.textBox2.Text += smsg[1] + " 上线了" + "\r\n";
+                    }
+                    
+                }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ip=this.txtIP.Text;
+            UdpClient uc = new UdpClient();
+            string msg = "PUBLIC|" + this.txtmsg.Text+"|"+this.txtName.Text;
+            byte[] bmsg = Encoding.Default.GetBytes(msg);
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Parse(ip),9527);
+            uc.Send(bmsg,bmsg.Length,ipep);
+        }
+    }
+}
